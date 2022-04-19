aa0c9b4f6e0a5ffd5f81051cc29852caf1fcdf8b
JordiWP <jgtorcal@gmail.com>
2022-04-19 21:40:04 +0200

    urls

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ Repositorio del Proyecto final DAW
 
 1. Crear la BBDD a partir del archivo SQL en el root
 
-2. Cambiar la URL de la aplicación en /admin/config/config.php (constante FRONTURL)
+2. Hacer un GIT clone del proyecto en la carpeta deseada dentro del directorio httpdocs de Apache
 
-3. Hacer un GIT clone del proyecto en la carpeta dentro del directorio httpdocs de Apache
+3. Editar el archivo config.php del directorio /admin/config de la siguiente forma según el nombre de la carpeta
+
+´´´
+define('PROJECT_FOLDER', 'vayacar');
+define('FRONTURL', 'http://localhost/vayacar/');
+```
--- admin/config/config.php
+++ admin/config/config.php
@@ -1,21 +1,15 @@
 <?php
 
-// Ejemplo: http://localhost/vayacar/
 define('PROJECT_FOLDER', 'vayamec');
 define('FRONTURL', 'http://localhost/vayamec/');
-
 
 /* CONSTANTES APP */
 define('APPURL', FRONTURL .'admin/');
 define('PUBLICURL', APPURL . 'views/public/');// Carpeta donde están los CSS para el layout
-
 define('UPLOADS_URL', PROJECT_FOLDER.'/admin/views/public/uploads/');// Carpeta para subir los logos
 define('UPLOADS_COCHES_URL', PROJECT_FOLDER.'/admin/views/public/uploads/coches/');// Carpeta para subir las fotos de los coches
-
-
 define('PUBLIC_UPLOADS_URL', APPURL.'views/public/uploads/');// Carpeta pública donde están las imágenes de logos para las vistas
 define('PUBLIC_UPLOADS_COCHES_URL', APPURL.'views/public/uploads/coches/');// Carpeta pública donde están las imágenes de los coches para las vistas
-
 
 /* CONSTANTES DB */
 define('DB_NAME', 'vayamec');
